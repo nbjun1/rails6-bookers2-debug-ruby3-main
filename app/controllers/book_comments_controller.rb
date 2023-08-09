169738e4c4e2769047e07958af13f830dbace370
nbjun1 <ec2-user@ip-172-31-24-145.ap-northeast-1.compute.internal>
2023-08-09 11:56:56 +0000

    0809 21

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    @user =@book.user
+    @user = @book.user
     @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = book.id
+    @book_comment.book_id = @book.id
     @book_comment.save
     #redirect_to book_path(@book)
   end
 
   def destroy
-    @book_comment = BookComment.find(params[:id]).destroy
+    @book_comment = BookComment.find(params[:id])
     @book_comment.destroy
     @book = Book.find(params[:book_id])
      #redirect_to request.referer
