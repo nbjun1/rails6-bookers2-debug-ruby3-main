641b9daa6ac9c2f23b0cccb89860d77f0bb1e66a
nbjun1 <ec2-user@ip-172-31-24-145.ap-northeast-1.compute.internal>
2023-08-09 07:33:03 +0000

    0809 16

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @user =@book.user
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = book.id
+    @book_comment.save
+    #redirect_to book_path(@book)
   end
 
   def destroy
-    @book_comment = BookComment.find(params[:id])
-    if @book_comment.user == current_user
-     @book_comment.destroy
-     redirect_to book_path(@book_comment.book)
-    end
+    @book_comment = BookComment.find(params[:id]).destroy
+    @book_comment.destroy
+    @book = Book.find(params[:book_id])
+     #redirect_to request.referer
   end
 
   private
