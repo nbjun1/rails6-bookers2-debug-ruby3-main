14de322d8dda2c8582859a86d0428c53c5bfe7d4
nbjun1 <x42revolution@gmail.com>
2023-11-02 14:48:52 +0000

    DM

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+  belongs_to :user
+  validate :message, presence:true
+  def other_user(current_user)
+    sender == current_user ? recipient : sender
+  end
 end
