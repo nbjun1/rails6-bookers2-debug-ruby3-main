ab3e51f1c5c55b767124749934e360a6525ce22c
nbjun1 <x42revolution@gmail.com>
2023-11-03 14:43:15 +0000

    DM

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
+
+  belongs_to :sender, class_name: 'User'
+  belongs_to :recipient, class_name: 'User'
   validate :message, presence:true
   def other_user(current_user)
     sender == current_user ? recipient : sender
