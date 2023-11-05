268e6656da3a64aca64fafe0b5370dd5d8b2d706
nbjun1 <x42revolution@gmail.com>
2023-11-05 14:42:54 +0000

    一時保存

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
 
   belongs_to :sender, class_name: 'User'
   belongs_to :recipient, class_name: 'User'
-  validate :message, presence:true
+  validates :message, presence:true
   def other_user(current_user)
     sender == current_user ? recipient : sender
   end
