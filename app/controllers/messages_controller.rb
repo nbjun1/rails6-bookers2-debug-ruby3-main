1864fd4e01343bed41f0c9a659eed761d2365159
nbjun1 <x42revolution@gmail.com>
2023-11-11 12:00:02 +0000

    DM

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,27 +1,30 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_message, only: [:show]
 
   def index
     @users = User.all
   end
 
   def show
-    @reciver = User.find(params[:id])
-    #@messages = current_user.messages_between(@reciver)
+    @received_messages = current_user.received_messages
+    @sent_messages = current_user.sent_messages
+
   end
 
   def create
-    @recipient = User.find(params[:recipient_id])
-    @message = current_user.send_message(@recipient, params[:content])
-
-    respond_to do |format|
-      format.js
-    end
+    @message = current_user.sent_messages.build(message_params)
+    @message.save
+    redirect_to messages_path
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:message)
+    params.require(:message).permit(:message, :receiver_id)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
